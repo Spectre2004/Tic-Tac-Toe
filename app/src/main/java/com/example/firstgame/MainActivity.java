04c662a188c3559e10741cfbf76b7da068a3016b
Spectre <matuhinv27@gmail.com>
2022-11-10 10:52:02 +0300

    TicTac

--- app/src/main/java/com/example/firstgame/MainActivity.java
+++ app/src/main/java/com/example/firstgame/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                     btnList[i][j].setEnabled(false);
                 }
             }
-
+        //xs
         }
         else if (btnList[1][0].getText() == "X" && btnList[1][1].getText() == "X" && btnList[1][2].getText() == "X")
         {
